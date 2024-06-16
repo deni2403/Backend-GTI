edc054d48468542cd7aa7133520f0a79ddd20d8b
deni2403 <deni240397@gmail.com>
2024-06-17 02:17:27 +0700

    update config env

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ const pg = require('pg');
 
 const config = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     dialectModule: pg,
     port: process.env.DB_PORT,
@@ -18,10 +18,10 @@ const config = {
     },
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     dialectModule: pg,
     port: process.env.DB_PORT,
@@ -33,13 +33,13 @@ const config = {
     },
   },
   production: {
-    username: process.env.DB_USERNAME_PROD,
-    password: process.env.DB_PASSWORD_PROD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST_PROD,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     dialectModule: pg,
-    port: process.env.DB_PORT_PROD,
+    port: process.env.DB_PORT,
     dialectOptions: {
       ssl: {
         require: true,
