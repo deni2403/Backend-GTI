d6201b35cc7de07a749877b28bd33692ef20f224
deni2403 <deni240397@gmail.com>
2024-06-23 05:45:25 +0700

    Update Cronjobs

--- controllers/cronJobController.js
+++ controllers/cronJobController.js
@@ -1,22 +1,11 @@
-const { Client } = require('pg');
+const { Sequelize, container } = require('../models');
 
-const updateIddleDays = async (req, res) => {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-
+const updateIddleDays = async () => {
   try {
-    await client.connect();
-    await client.query('UPDATE containers SET iddle_days = iddle_days + 1');
-    await client.end();
-    res.status(200).send('iddle_days updated successfully');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error updating iddle_days');
+    await container.update({ iddle_days: Sequelize.literal('iddle_days + 1') });
+    console.log('iddle_days updated successfully');
+  } catch (error) {
+    console.error('Error updating iddle_days:', error);
   }
 };
 
