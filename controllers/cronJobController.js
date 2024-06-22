05aaa17a7ff686f1fea71097fea0614e0cbbd112
deni2403 <deni240397@gmail.com>
2024-06-23 06:48:07 +0700

    Update Cron Jobs Config

--- controllers/cronJobController.js
+++ controllers/cronJobController.js
@@ -2,7 +2,10 @@ const { Sequelize, container } = require('../models');
 
 const updateIddleDays = async () => {
   try {
-    await container.update({ iddle_days: Sequelize.literal('iddle_days + 1') });
+    await container.update(
+      { iddle_days: Sequelize.literal('iddle_days + 1') },
+      { where: {} },
+    );
     console.log('iddle_days updated successfully');
   } catch (error) {
     console.error('Error updating iddle_days:', error);
