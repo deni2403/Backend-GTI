74eb4333001316dbe918040ffd46e8dfb8e62796
deni2403 <deni240397@gmail.com>
2024-06-23 20:40:20 +0700

    Testing Trigger Time 3 & Cronjob logging time

--- controllers/cronJobController.js
+++ controllers/cronJobController.js
@@ -2,13 +2,17 @@ const { Sequelize, container } = require('../models');
 
 const updateIddleDays = async () => {
   try {
+    const startTime = new Date();
     await container.update(
       { iddle_days: Sequelize.literal('iddle_days + 1') },
       { where: {}, logging: false },
     );
-    response.status(200).json({ success: true });
+    const endTime = new Date();
+    console.log(
+      `updateIddleDays cronjob completed in ${endTime - startTime} ms`,
+    );
   } catch (error) {
-    response.status(500).json({ success: false, error });
+    console.error('Failed to update iddle days:', error);
   }
 };
 
