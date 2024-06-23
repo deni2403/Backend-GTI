040f232a3fc38bfe1fd040778d1c172704a34998
deni2403 <deni240397@gmail.com>
2024-06-23 07:23:15 +0700

    Update Cron Jobs route

--- routers/route.js
+++ routers/route.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const users = require('./usersRoute')
-const repair = require('./repairRoute')
-const containers = require('./containersRoute')
-const shipments = require('./shipmentsRoute')
-const authentication = require('../middlewares/authentication')
-const userAuthorization = require('../middlewares/authorization')
-const { getLogActivity } = require('../controllers/log_activityController')
-const {login, currentUser} = require('../controllers/userController')
-const {updateIddleDays} = require('../controllers/cronJobController')
+const express = require('express');
+const router = express.Router();
+const users = require('./usersRoute');
+const repair = require('./repairRoute');
+const containers = require('./containersRoute');
+const shipments = require('./shipmentsRoute');
+const authentication = require('../middlewares/authentication');
+const userAuthorization = require('../middlewares/authorization');
+const { getLogActivity } = require('../controllers/log_activityController');
+const { login, currentUser } = require('../controllers/userController');
+const { updateIddleDays } = require('../controllers/cronJobController');
 
+router.post('/login', login);
+router.get('/updateIddleDays', updateIddleDays);
+router.use(authentication);
+router.get('/getMe', currentUser);
+router.get('/logs', userAuthorization, getLogActivity);
+router.use(users, repair, containers, shipments);
 
-router.post('/login',login)
-router.post('/updateIddleDays', updateIddleDays);
-router.use(authentication)
-router.get('/getMe',currentUser)
-router.get('/logs',userAuthorization,getLogActivity)
-router.use(users,repair,containers,shipments)
-
-module.exports= router
+module.exports = router;
